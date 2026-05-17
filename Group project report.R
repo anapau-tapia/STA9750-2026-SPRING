783ab5ac4720d2afb904cbaafbe2e19ab3d0cbaa
Ana Paula Tapia <ana.tapia1@baruchmail.cuny.edu>
2026-05-17 15:08:33 -0400

    commit

---
+++ Group project report.R
@@ -0,0 +1,14 @@
+#notes of the team project: 
+#  *The Data Hierarchy
+#In the data you are using, Agriculture is not just about plants; it is actually dominated by animal biology. It is comprised of three main sub-categories:
+  
+ # Enteric Fermentation: This is the methane produced by the digestive systems of ruminants (cows belching).
+
+#Manure Management: Methane released from stored animal waste.
+
+#Rice Cultivation/Crop Burning: While these are "plant-based," they represent a much smaller fraction of U.S. methane compared to the first two.
+
+#By selecting Agriculture, you are capturing over 95% of all livestock-related methane in the U.S.
+
+# While Agriculture accounts for roughly 10% of total U.S. greenhouse gases,it is responsible for nearly 40% of U.S.Methane emissions. Our analysis focuses
+# on Methane specifically because it is a more potent 'short-lived' climate pollutant where the livestock industry has a disproportionately large impact.
